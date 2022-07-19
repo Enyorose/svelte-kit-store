c162478c860520012c79f10b99acde13a79b2ef6
Enyorose <jessica.watson1@live.com>
2022-07-19 14:09:30 -0600

    set up data in store

---
+++ my-app/src/lib/models/userModel.ts
@@ -0,0 +1,11 @@
+export interface PermitVehicleModel {
+    vehicle_id: string;
+    user_id: string;
+    vehicle_make: string;
+    vehicle_model: string;
+    vehicle_year: string;
+    vehicle_colour: string;
+    vehicle_plate: string;
+    vehicle_plate_province: string;
+    vehicle_other: string;
+  }
---
+++ my-app/src/lib/stores/store.ts
@@ -0,0 +1,25 @@
+import type { PermitVehicleModel } from "../models/userModel";
+import { readable, writable } from "svelte/store";
+
+export let vehicles = writable(<PermitVehicleModel[]>[{
+    vehicle_id: "j679hjk",
+    user_id: "kjsi879",
+    vehicle_make: "Ford",
+    vehicle_model: "Escort",
+    vehicle_colour: "Blue",
+    vehicle_year: "2019",
+    vehicle_plate: "ABC123",
+    vehicle_plate_province: "AB",
+    vehicle_other: "",
+  },{
+    vehicle_id: "wi8hjk",
+    user_id: "kjsi879",
+    vehicle_make: "Subaru",
+    vehicle_model: "Outback",
+    vehicle_colour: "Black",
+    vehicle_year: "2016",
+    vehicle_plate: "jw8hjk",
+    vehicle_plate_province: "AB",
+    vehicle_other: "",
+  }
+],);
